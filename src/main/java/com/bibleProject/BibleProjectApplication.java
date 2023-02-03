ec2c41272bd768317b8db7d90e9564d10bf7032d
ksol46 <doomee880406@naver.com>
2023-02-03 17:58:07 +0900

    up

---
+++ src/main/java/com/bibleProject/BibleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bibleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibleProject/BibleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
