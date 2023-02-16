495b48c372b82a516443b10ff61bd2eea932750f
ksol46 <doomee880406@naver.com>
2023-02-16 21:16:08 +0900

    d

--- src/main/java/com/bibleProject/BibleProjectApplication.java
+++ src/main/java/com/bibleProject/BibleProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
-public class BibleProjectApplication {
+public class BibleProjectApplication { //
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibleProjectApplication.class, args);
